0019189ee48faf3d7cabcf8bbf32841567d4226b
Julien Thiphineau <j.thiphineau@yahoo.fr>
2023-03-16 11:32:34 +0100

    first commit

---
+++ src/test/java/com/doranco/flash/Tp1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doranco.flash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
