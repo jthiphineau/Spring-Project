7bfacfd338f348485d669948eabb5ce56d4c23f3
Julien Thiphineau <j.thiphineau@yahoo.fr>
2023-03-16 16:58:29 +0100

    Ajout test, test2, create dans le controller

--- src/main/java/com/doranco/flash/controller/SpecialiteController.java
+++ src/main/java/com/doranco/flash/controller/SpecialiteController.java
@@ -1,15 +1,34 @@
 package com.doranco.flash.controller;
 
+import com.doranco.flash.dto.SpecialiteDto;
+import com.doranco.flash.service.SpecialiteService;
+import com.doranco.flash.util.CustomResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //cette classe est une ressource REST
 @RequestMapping("api/v1/specialite")
 public class SpecialiteController {
+
+    private final SpecialiteService specialiteService;
+    //injection de la dependance par le constructeur
+    public SpecialiteController(SpecialiteService specialiteService) {
+        this.specialiteService = specialiteService;
+    }
+
     @GetMapping("/test")
-    public ResponseEntity<String> testController(){
+    public ResponseEntity<?> testController1(){
         return ResponseEntity.status(200).body("Test du controller");
     }
+    @GetMapping("/test2")
+    public ResponseEntity<?> testController2(){
+        return CustomResponse.errorResponse(400,"Le titre existe deja");
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody SpecialiteDto specialiteDto){
+        return specialiteService.create(specialiteDto);
+
+    }
+
 }
--- src/main/java/com/doranco/flash/service/impl/SpecialiteServiceImpl.java
+++ src/main/java/com/doranco/flash/service/impl/SpecialiteServiceImpl.java
@@ -1,20 +1,37 @@
 package com.doranco.flash.service.impl;
 
 import com.doranco.flash.dto.SpecialiteDto;
+import com.doranco.flash.entity.Specialite;
 import com.doranco.flash.repository.SpecialiteRepository;
 import com.doranco.flash.service.SpecialiteService;
+import com.doranco.flash.util.CustomResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpecialiteServiceImpl implements SpecialiteService {
     private final SpecialiteRepository specialiteRepository;
+    private final ModelMapper modelMapper; //permet de transformer dto en entity et entity en dto
 
     public SpecialiteServiceImpl(SpecialiteRepository specialiteRepository) {
         this.specialiteRepository = specialiteRepository;
+        this.modelMapper = new ModelMapper();
     }
 
     @Override
     public ResponseEntity<?> create(SpecialiteDto specialiteDto) {
-        return null;
+        // TODO validation des attributs
+        var specialite =  modelMapper.map(specialiteDto, Specialite.class);
+        try{
+            specialiteRepository.save(specialite);
+            //convertir entity en dtp
+            specialiteDto = modelMapper.map(specialite, SpecialiteDto.class);
+        }catch (Exception ex){
+            return CustomResponse.errorResponse(500,ex.getMessage());
+
+        }
+        return CustomResponse.succesResponse(201,specialiteDto);
     }
 
     @Override
